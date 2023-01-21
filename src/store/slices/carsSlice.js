9f0c0599f8c885a18fb5ff76b549f7985d7e3065
luka0009 <109582754+luka0009@users.noreply.github.com>
2023-01-21 23:36:50 +0400

    Update carsSlice.js

--- src/store/slices/carsSlice.js
+++ src/store/slices/carsSlice.js
@@ -1,30 +1,39 @@
-import { createSlice, nanoid } from '@reduxjs/toolkit';
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  searchTerm: "",
+  data: [],
+};
+
+const cars =
+  localStorage.getItem("cars") !== null
+    ? JSON.parse(localStorage.getItem("cars"))
+    : initialState;
 
 const carsSlice = createSlice({
-  name: 'cars',
-  initialState: {
-    searchTerm: '',
-    data: [],
-  },
+  name: "cars",
+  initialState: cars,
   reducers: {
     changeSearchTerm(state, action) {
       state.searchTerm = action.payload;
     },
     addCar(state, action) {
-      state.data.push({
+      state.data?.push({
         name: action.payload.name,
         cost: action.payload.cost,
         id: nanoid(),
       });
+      if (initialState) localStorage.setItem("cars", JSON.stringify(state));
     },
     removeCar(state, action) {
       const updated = state.data.filter((car) => {
         return car.id !== action.payload;
       });
       state.data = updated;
+      localStorage.setItem("cars", JSON.stringify(state));
     },
   },
 });
 
-export const { changeSearchTerm, addCar, removeCar } = carsSlice.actions;
+export const { changeSearchTerm, addCar, removeCar, editCar } = carsSlice.actions;
 export const carsReducer = carsSlice.reducer;
